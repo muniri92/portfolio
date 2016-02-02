9dc16c3fd8540f4391b38d70b88b196c11e90e4d
muniri92 <MunirIbrahim@Munirs-MacBook-Pro.local>
2016-02-02 12:05:00 -0800

    didn't change too much. messed with some CSS

---
+++ js/app.js
@@ -0,0 +1,31 @@
+var projects = [];
+function Project (opts) {
+  this.title = opts.title;
+  this.image = 'img/' + opts.image;
+  this.gitRepo = opts.gitRepo;
+  this.url = opts.url;
+  this.body = opts.body;
+};
+Project.prototype.toHtml = function () {
+  var $newProject = $('article.template').clone();
+  $newProject.removeClass('template');
+  $newProject.find('.img').html('<img src="' + this.image + '">');
+  $newProject.find('header > h1').text(this.title);
+
+  $newProject.find('#ghRepo a').attr('href', this.gitRepo);
+  // html('<a href= "' + this.gitRepo + '">GitHub Repo</a>');
+
+  // $newProject.find('#ghRepo').html('<a href= "' + this.gitRepo + '">GitHub Repo</a>');
+  $newProject.find('#website').html('<a href= "' + this.url + '">Project Link</a>');
+  $newProject.find('.site-body').html(this.body);
+  $newProject.append('<hr>');
+  return $newProject;
+};
+
+
+rawProject.forEach(function(ele) {
+  projects.push(new Project(ele));
+});
+projects.forEach(function(a) {
+  $('#projects').append(a.toHtml());
+});
---
+++ js/projects.js
@@ -0,0 +1,16 @@
+var rawProject = [
+  {
+    title: 'Busmall',
+    image: 'busmall.png',
+    gitRepo: 'http://muniri92.github.io/busmall/',
+    url: 'https://github.com/muniri92/busmall',
+    body: '<p>HEYHEYHEY</p>'
+  },
+  {
+    title: 'Equip',
+    image: 'equip.png',
+    gitRepo: 'https://github.com/heyduckd/equip',
+    url: 'http://heyduckd.github.io/equip/',
+    body: '<p>HEYHEYHEY</p>'
+  },
+];
