dee1013bcc5ac1f2a64bff7eb6a422c456e62bf4
muniri92 <MunirIbrahim@Munirs-MacBook-Pro.local>
2016-02-03 23:22:52 -0800

    got the media query to work properly for my nav bar

--- js/projectView.js
+++ js/projectView.js
@@ -1,57 +1,4 @@
 var projectView = {};
-// articleView.populateFilters = function() {
-//   $('article').each(function() {
-//     if (!$(this).hasClass('template')) {
-//       var val = $(this).find('address a').text();
-//       var optionTag = '<option value="' + val + '">' + val + '</option>';
-//       $('#author-filter').append(optionTag);
-//       val = $(this).attr('data-category');
-//       optionTag = '<option value="' + val + '">' + val + '</option>';
-//       if ($('#category-filter option[value="' + val + '"]').length === 0) {
-//         $('#category-filter').append(optionTag);
-//       }
-//     }
-//   });
-// };
-// articleView.handleAuthorFilter = function() {
-//   $('#author-filter').on('change', function() {
-//     if ($(this).val()) {
-//       $('article').hide();
-//       $('article[data-author = "' + $(this).val() + '"]').show();
-//     } else {
-//       $('article').show();
-//       $('article.template').hide();
-//     }
-//     $('#category-filter').val('');
-//   });
-// };
-// articleView.handleCategoryFilter = function() {
-//   $('#category-filter').on('change', function() {
-//     if ($(this).val()) {
-//       $('article').hide();
-//       $('article[data-category = "' + $(this).val() + '"]').show();
-//     } else {
-//       $('article').show();
-//       $('article.template').hide();
-//     }
-//     $('#author-filter').val('');
-//   });
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 projectView.handleMainNav = function() {
   $('.main_nav').on('click', 'li', function() {
@@ -63,27 +10,7 @@ projectView.handleMainNav = function() {
 };
 projectView.handleMainNav();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//
 // articleView.setTeasers = function() {
 //   $('.article-body *:nth-of-type(n+2)').hide();
 //   $('.read-on').on('click', function(e) {
@@ -92,17 +19,4 @@ projectView.handleMainNav();
 //     $(this).hide();
 //   });
 // };
-
-
-
-
-
-
-
-
-
-// articleView.populateFilters();
-// articleView.handleAuthorFilter();
-// articleView.handleCategoryFilter();
-// projectView.handleMainNav();
 // articleView.setTeasers();
