ba1ddadbc38510516360c82871a2e4f1a90b1c94
muniri92 <MunirIbrahim@Munirs-MacBook-Pro.local>
2016-02-03 20:25:42 -0800

    I got good outline, now gotta start working towards putting css in it's corret files and media query to get some responsive design

--- js/projects.js
+++ js/projects.js
@@ -2,14 +2,14 @@ var rawProject = [
   {
     title: 'Busmall',
     image: 'busmall.png',
-    gitRepo: 'http://muniri92.github.io/busmall/',
-    url: 'https://github.com/muniri92/busmall',
+    gitRepo: 'https://github.com/muniri92/busmall',
+    url: 'http://muniri92.github.io/busmall/',
     body: '<p>HEYHEYHEY</p>'
   },
   {
     title: 'Equip',
     image: 'equip.png',
-    gitRepo: 'https://github.com/heyduckd/equip',
+    gitRepo: 'https://github.com/muniri92/equip',
     url: 'http://heyduckd.github.io/equip/',
     body: '<p>HEYHEYHEY</p>'
   },
