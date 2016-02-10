36459ba101eee99f7f64c063e6307f940a5240f8
muniri92 <MunirIbrahim@Munirs-MacBook-Pro.local>
2016-02-09 16:48:34 -0800

    pull everything in an IFFE

--- js/projectView.js
+++ js/projectView.js
@@ -1,47 +1,50 @@
-var projectView = {};
-var educationView = {};
+(function(module) {
+  var projectView = {};
+  var educationView = {};
 
-// SWITCHING BETWEEN ABOUT AND PROJECT TAB
-projectView.handleMainNav = function() {
-  $('.main_nav').on('click', 'li', function() {
-    $('.tab_content').hide();
-    $('#' + $(this).data('link') + '').show();
-  });
-  $('.main_nav .tab:first').click();
-};
+  // SWITCHING BETWEEN ABOUT AND PROJECT TAB
+  projectView.handleMainNav = function() {
+    $('.main_nav').on('click', 'li', function() {
+      $('.tab_content').hide();
+      $('#' + $(this).data('link') + '').show();
+    });
+    $('.main_nav .tab:first').click();
+  };
 
-projectView.initIndexPage = function() {
-  Project.all.forEach(function(a){
-    $('#project').append(a.toHtml());
-  });
-  projectView.handleMainNav();
-};
+  projectView.initIndexPage = function() {
+    Project.all.forEach(function(a){
+      $('#project').append(a.toHtml());
+    });
+    projectView.handleMainNav();
+  };
 
-educationView.initIndexPage = function() {
-  Education.all.forEach(function(a){
-    $('#edu').append(a.toHtml());
-  });
-};
+  educationView.initIndexPage = function() {
+    Education.all.forEach(function(a){
+      $('#edu').append(a.toHtml());
+    });
+  };
 
+  // CREATING THE 'READ ON' FUNCTION
+  projectView.setTeasers = function() {
+    $('.project_body *:nth-of-type(n+2)').hide();
+    $('.read_on').on('click', function(e) {
+      e.preventDefault();
+      $(this).sibling('.project_body').children().show();
+      $(this).hide();
+    });
+  };
 
+  // projectView.setTeasers = function() {
+  //   $('.project_body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any artcile body.
+  //
+  //   $('#project').on('click', 'a.read-on', function(e) {
+  //     e.preventDefault();
+  //     $(this).parent().find('*').fadeIn();
+  //     $(this).hide();
+  //   });
+  // };
+  projectView.setTeasers();
 
-// CREATING THE 'READ ON' FUNCTION
-projectView.setTeasers = function() {
-  $('.project_body *:nth-of-type(n+2)').hide();
-  $('.read_on').on('click', function(e) {
-    e.preventDefault();
-    $(this).sibling('.project_body').children().show();
-    $(this).hide();
-  });
-};
-
-// projectView.setTeasers = function() {
-//   $('.project_body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any artcile body.
-//
-//   $('#project').on('click', 'a.read-on', function(e) {
-//     e.preventDefault();
-//     $(this).parent().find('*').fadeIn();
-//     $(this).hide();
-//   });
-// };
-// projectView.setTeasers();
+  module.projectView = projectView;
+  module.educationView = educationView;
+})(window);
