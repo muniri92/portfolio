335ab11c83e567f5c911bdb871ce56921e108874
muniri92 <MunirIbrahim@Munirs-MacBook-Pro.local>
2016-02-12 09:05:20 -0800

    attempting to refactor

--- js/app.js
+++ js/app.js
@@ -3,142 +3,213 @@
   function Generic (opts) {
     for (var i in opts) {
       this[i] = opts[i];
+      console.log(this[i]);
     }
   };
 
   Generic.all = [];
 
   /////////////////////////////////////////////////////////
-  function Project (opts) {
-    for (var i in opts) {
-      this[i] = opts[i];
-    }
-  };
+  // function Project (opts) {
+  //   for (var i in opts) {
+  //     this[i] = opts[i];
+  //   }
+  // };
 
-  function Education (edu) {
-    for (var i in edu) {
-      this[i] = edu[i];
-    }
-  };
+  // function Education (edu) {
+  //   for (var i in edu) {
+  //     this[i] = edu[i];
+  //   }
+  // };
+  //
+  // Project.all = [];
+  // Education.all = [];
 
-  Project.all = [];
-  Education.all = [];
   /////////////////////////////////////////////////////////
 
-  Project.prototype.toHtml = function() {
-    var template = Handlebars.compile($('#project_template').text());
 
-    this.daysAgo = parseInt((new Date() - new Date(this.startDate))/60/60/24/1000);
-    this.publishStatus = this.startDate ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-
-    return template(this);
-  };
-
-  Education.prototype.toHtml = function() {
+  Generic.prototype.toHtml = function(template) {
     var template = Handlebars.compile($('#edu_template').text());
     return template(this);
   };
 
+
+  // Project.prototype.toHtml = function() {
+  //   var template = Handlebars.compile($('#project_template').text());
+  //
+  //   this.daysAgo = parseInt((new Date() - new Date(this.startDate))/60/60/24/1000);
+  //   this.publishStatus = this.startDate ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  //
+  //   return template(this);
+  // };
+  //
+  // Education.prototype.toHtml = function() {
+  //   var template = Handlebars.compile($('#edu_template').text());
+  //   return template(this);
+  // };
+
   /////////////////////////////////////////////////////////
 
-  Project.loadAll = function(rawProject) {
-    rawProject.sort(function(a,b) {
-      return (new Date(b.startDate)) - (new Date(a.startDate));
-    });
-    rawProject.forEach(function(e) {
-      Project.all.push(new Project(e));
-      console.log(Project.all);
+  Generic.loadAll = function(rawGeneric) {
+    rawGeneric.forEach(function(e) {
+      Generic.all.push(new Generic(e));
+      console.log(e);
     });
   };
 
 
-  Education.loadAll = function(rawEducation) {
-    rawEducation.forEach(function(e) {
-      Education.all.push(new Education(e));
-    });
-  };
+  // Project.loadAll = function(rawProject) {
+  //   rawProject.sort(function(a,b) {
+  //     return (new Date(b.startDate)) - (new Date(a.startDate));
+  //   });
+  //   rawProject.forEach(function(e) {
+  //     Project.all.push(new Project(e));
+  //     console.log(Project.all);
+  //   });
+  // };
+  //
+  //
+  // Education.loadAll = function(rawEducation) {
+  //   rawEducation.forEach(function(e) {
+  //     Education.all.push(new Education(e));
+  //   });
+  // };
 
   /////////////////////////////////////////////////////////
 
-  Project.fetchAll = function() {
-    if (localStorage.rawProject) {
+  var url;
+  $('#gen_proj').on('click', function() {
+    url = 'project.json';
+  });
+  $('#gen_resume').on('click', function() {
+    url = 'edu.json';
+  });
+  Generic.fetchAll = function(url) {
+    if (localStorage.rawGeneric) {
       $.ajax({
         type: 'HEAD',
-        url: 'data/project.json',
+        url: 'data/' + url,
         success: function(data, message, xhr) {
-          Project.loadAll(JSON.parse(localStorage.rawProject));
+          Generic.loadAll(JSON.parse(localStorage.rawGeneric));
           var eTag = xhr.getResponseHeader('eTag');
           if (localStorage.eTag || eTag !== localStorage.eTag) {
             localStorage.eTag = eTag;
           } else {
-            Project.loadAll(JSON.parse(localStorage.rawProject));
-            projectView.initIndexPage();
+            Generic.loadAll(JSON.parse(localStorage.rawGeneric));
+            genericView.initIndexPage();
           }
         }
       });
-      Project.loadAll(JSON.parse(localStorage.rawProject));
-      projectView.initIndexPage();
+      // Generic.loadAll(JSON.parse(localStorage.rawGeneric));
+      genericView.initIndexPage();
+
     } else {
-
-      $.getJSON('data/project.json', function(data) {
-        localStorage.setItem('rawProject', JSON.stringify(data));
+      $.getJSON('data/' + url, function(data) {
+        localStorage.setItem('rawGeneric', JSON.stringify(data));
       });
       $.ajax({
         type: 'HEAD',
-        url: 'data/project.json',
+        url: 'data/' + url,
         success: function(data, message, xhr) {
-          Project.loadAll(JSON.parse(localStorage.rawProject));
+          Generic.loadAll(JSON.parse(localStorage.rawGeneric));
           var eTag = xhr.getResponseHeader('eTag');
           if (localStorage.eTag || eTag !== localStorage.eTag) {
             localStorage.eTag = eTag;
           } else {
-            Project.loadAll(JSON.parse(localStorage.rawProject));
-            projectView.initIndexPage();
+            Generic.loadAll(JSON.parse(localStorage.rawGeneric));
+            genericView.initIndexPage();
           }
         }
       });
-      Project.loadAll(JSON.parse(localStorage.rawProject));
-      projectView.initIndexPage();
+      Generic.loadAll(JSON.parse(localStorage.rawGeneric));
+      genericView.initIndexPage();
+      // console.log(rawGeneric);
     }
   };
 
-  Education.fetchAll = function() {
-    if (localStorage.rawEducation) {
-      $.ajax({
-        type: 'HEAD',
-        url: 'data/edu.json',
-        success: function(data, message, xhr) {
-          Education.loadAll(JSON.parse(localStorage.rawEducation));
-          var eTag = xhr.getResponseHeader('eTag');
-          if (localStorage.eTag || eTag !== localStorage.eTag) {
-            localStorage.eTag = eTag;
-          } else {
-            Education.loadAll(JSON.parse(localStorage.rawEducation));
-            educationView.initIndexPage();
-          }
-        }
-      });
-      Education.loadAll(JSON.parse(localStorage.rawEducation));
-      educationView.initIndexPage();
-    } else {
-      $.getJSON('data/edu.json', function(data) {
-        localStorage.setItem('rawEducation', JSON.stringify(data));
-      });
-      $.ajax({
-        type: 'HEAD',
-        url: 'data/edu.json',
-        success: function(data, message, xhr) {
-          Education.loadAll(JSON.parse(localStorage.rawEducation));
-        }
-      });
-      Education.loadAll(JSON.parse(localStorage.rawEducation));
-      educationView.initIndexPage();
-    }
-  };
+  //
+  //
+  // Project.fetchAll = function() {
+  //   if (localStorage.rawProject) {
+  //     $.ajax({
+  //       type: 'HEAD',
+  //       url: 'data/project.json',
+  //       success: function(data, message, xhr) {
+  //         Project.loadAll(JSON.parse(localStorage.rawProject));
+  //         var eTag = xhr.getResponseHeader('eTag');
+  //         if (localStorage.eTag || eTag !== localStorage.eTag) {
+  //           localStorage.eTag = eTag;
+  //         } else {
+  //           Project.loadAll(JSON.parse(localStorage.rawProject));
+  //           projectView.initIndexPage();
+  //         }
+  //       }
+  //     });
+  //     Project.loadAll(JSON.parse(localStorage.rawProject));
+  //     projectView.initIndexPage();
+  //   } else {
+  //
+  //     $.getJSON('data/project.json', function(data) {
+  //       localStorage.setItem('rawProject', JSON.stringify(data));
+  //     });
+  //     $.ajax({
+  //       type: 'HEAD',
+  //       url: 'data/project.json',
+  //       success: function(data, message, xhr) {
+  //         Project.loadAll(JSON.parse(localStorage.rawProject));
+  //         var eTag = xhr.getResponseHeader('eTag');
+  //         if (localStorage.eTag || eTag !== localStorage.eTag) {
+  //           localStorage.eTag = eTag;
+  //         } else {
+  //           Project.loadAll(JSON.parse(localStorage.rawProject));
+  //           projectView.initIndexPage();
+  //         }
+  //       }
+  //     });
+  //     Project.loadAll(JSON.parse(localStorage.rawProject));
+  //     projectView.initIndexPage();
+  //   }
+  // };
+  //
+  // Education.fetchAll = function() {
+  //   if (localStorage.rawEducation) {
+  //     $.ajax({
+  //       type: 'HEAD',
+  //       url: 'data/edu.json',
+  //       success: function(data, message, xhr) {
+  //         Education.loadAll(JSON.parse(localStorage.rawEducation));
+  //         var eTag = xhr.getResponseHeader('eTag');
+  //         if (localStorage.eTag || eTag !== localStorage.eTag) {
+  //           localStorage.eTag = eTag;
+  //         } else {
+  //           Education.loadAll(JSON.parse(localStorage.rawEducation));
+  //           educationView.initIndexPage();
+  //         }
+  //       }
+  //     });
+  //     Education.loadAll(JSON.parse(localStorage.rawEducation));
+  //     educationView.initIndexPage();
+  //   } else {
+  //     $.getJSON('data/edu.json', function(data) {
+  //       localStorage.setItem('rawEducation', JSON.stringify(data));
+  //     });
+  //     $.ajax({
+  //       type: 'HEAD',
+  //       url: 'data/edu.json',
+  //       success: function(data, message, xhr) {
+  //         Education.loadAll(JSON.parse(localStorage.rawEducation));
+  //       }
+  //     });
+  //     Education.loadAll(JSON.parse(localStorage.rawEducation));
+  //     educationView.initIndexPage();
+  //   }
+  // };
 
   /////////////////////////////////////////////////////////
-  module.Project = Project;
-  module.Education = Education;
+  module.Generic = Generic;
+
+  // module.Project = Project;
+  // module.Education = Education;
   ////////////////////////////////////////////////////////
 
 }(window));
--- js/projectView.js
+++ js/projectView.js
@@ -1,9 +1,11 @@
 (function(module) {
-  var projectView = {};
-  var educationView = {};
+  var genericView = {};
+  //
+  // var projectView = {};
+  // var educationView = {};
 
   // SWITCHING BETWEEN ABOUT AND PROJECT TAB
-  projectView.handleMainNav = function() {
+  genericView.handleMainNav = function() {
     $('.main_nav').on('click', 'li', function() {
       $('.tab_content').hide();
       $('#' + $(this).data('link') + '').show();
@@ -11,18 +13,25 @@
     $('.main_nav .tab:first').click();
   };
 
-  projectView.initIndexPage = function() {
-    Project.all.forEach(function(a){
-      $('#project').append(a.toHtml());
-    });
-    projectView.handleMainNav();
-  };
-
-  educationView.initIndexPage = function() {
-    Education.all.forEach(function(a){
+  genericView.initIndexPage = function() {
+    Generic.all.forEach(function(a){
       $('#edu').append(a.toHtml());
     });
+    genericView.handleMainNav();
   };
+
+  // projectView.initIndexPage = function() {
+  //   Project.all.forEach(function(a){
+  //     $('#project').append(a.toHtml());
+  //   });
+  //   projectView.handleMainNav();
+  // };
+  //
+  // educationView.initIndexPage = function() {
+  //   Education.all.forEach(function(a){
+  //     $('#edu').append(a.toHtml());
+  //   });
+  // };
 
   // // CREATING THE 'READ ON' FUNCTION
   // projectView.setTeasers = function() {
@@ -45,6 +54,9 @@
   // };
   // projectView.setTeasers();
 
-  module.projectView = projectView;
-  module.educationView = educationView;
+  module.genericView = genericView;
+
+
+  // module.projectView = projectView;
+  // module.educationView = educationView;
 })(window);
